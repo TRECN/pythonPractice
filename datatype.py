309e6cca4e49107fc15b45bf47429201a3da96ab
TRECN <rishabh1103@outlook.com>
2022-09-17 17:37:12 +0530

    commit

--- datatype.py
+++ datatype.py
@@ -11,3 +11,20 @@ print(type(b))
 c=[1,2,"risahbh"]
 print (c,type(c))
 
+d={
+    1,2,4
+}
+print(d,type(d))
+
+e={
+    1:3,
+    "r":5
+}
+print(e,type(e))
+
+e=(1,2,"rishabh",[1,2,3])
+print(e,type(e))
+
+f=b'sfdfsdf121313fdsfsflskfjsklfjslk'
+print(f,type(f))
+
