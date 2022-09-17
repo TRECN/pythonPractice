5d715ae6b2c452a8a0e1b55ff8b3ecf47de383f1
TRECN <rishabh1103@outlook.com>
2022-09-17 17:38:29 +0530

    commit

--- datatype.py
+++ datatype.py
@@ -1,30 +1,39 @@
+#int
 x=10;
+#str
 y='6';
 z="rishabh";
+#float
 a=1.6;
+#complex
 b=3j;
 print(type(x) ,end=" ") 
 print(type(y))
 print(type(z),type(a))
 print(type(b))
 
+#list
 c=[1,2,"risahbh"]
 print (c,type(c))
 
+#set
 d={
     1,2,4
 }
 print(d,type(d))
 
+#dict
 e={
     1:3,
     "r":5
 }
 print(e,type(e))
 
+#tuple
 e=(1,2,"rishabh",[1,2,3])
 print(e,type(e))
 
+#bytes
 f=b'sfdfsdf121313fdsfsflskfjsklfjslk'
 print(f,type(f))
 
