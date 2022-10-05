0cbab9517e59b2551ec17d98ae1635fa91f5d904
TRECN <rishabh1103@outlook.com>
2022-10-05 22:03:22 +0530

    commit

--- assignment/exceptionHandling/zeroExcept.py
+++ assignment/exceptionHandling/zeroExcept.py
@@ -3,7 +3,8 @@ a=int(input())
 try:
     #exception may occur here
     print (100//a)
-    #no error part
-    print("no err")
 except ZeroDivisionError:
     print("ZeroDivisionError")
+#no error part
+else:
+    print("no err")
